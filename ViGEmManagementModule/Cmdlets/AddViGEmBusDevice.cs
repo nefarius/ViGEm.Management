b470e6233d9a1210996c6e4bfb1c6ff8bd37eada
Benjamin Höglinger <nefarius@dhmx.at>
2017-12-17 20:22:29 +0100

    Implemented Add-ViGEmBusDevice

--- ViGEmManagementModule/Cmdlets/AddViGEmBusDevice.cs
+++ ViGEmManagementModule/Cmdlets/AddViGEmBusDevice.cs
@@ -1,10 +1,44 @@
-﻿using System.Management.Automation;
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Runtime.InteropServices;
+using Nefarius.Devcon;
+using ViGEmManagementModule.Core;
 
 namespace ViGEmManagementModule.Cmdlets
 {
     [Cmdlet(VerbsCommon.Add, "ViGEmBusDevice")]
+    [OutputType(typeof(ViGEmBusDevice))]
     public class AddViGEmBusDevice : Cmdlet
     {
-        
+        [Parameter]
+        public string ClassName { get; set; } = "System";
+
+        [Parameter]
+        public Guid ClassGuid { get; set; } = ViGEmBusDevice.ClassGuid;
+
+        [Parameter]
+        public string HardwareId { get; set; } = "Root\\ViGEmBus\0\0";
+
+        protected override void ProcessRecord()
+        {
+            var ret = Devcon.Create(ClassName, ClassGuid, HardwareId);
+
+            if (ret)
+            {
+                WriteVerbose("Device node created successfully.");
+
+                ViGEmBusDevice.Devices.ToList().ForEach(WriteObject);
+            }
+            else
+            {
+                ThrowTerminatingError(new ErrorRecord(
+                    new Win32Exception(Marshal.GetLastWin32Error()),
+                    "Win32Exception",
+                    ErrorCategory.DeviceError,
+                    HardwareId));
+            }
+        }
     }
 }
